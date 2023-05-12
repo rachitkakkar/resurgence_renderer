c291528bb4717d3f328c040a7fe34418bcce5195
Rachit Kakkar <71226802+rachitkakkar@users.noreply.github.com>
2023-05-12 13:52:01 +0000

    Update Ray.py

--- Ray.py
+++ Ray.py
@@ -1,4 +1,4 @@
-class Ray: #Ray Structure
+class Ray: #cRay Structure
     def __init__(self, origin, direction):
-        self.origin = origin #Origin vector
-        self.direction = direction #Direction vector
+        self.origin = origin # Origin vector
+        self.direction = direction # Direction vector
