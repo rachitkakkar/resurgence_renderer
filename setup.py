205a81b223879efe33a7e4b8036c739b91f3cce1
Rachit Kakkar <71226802+rachitkakkar@users.noreply.github.com>
2023-05-12 00:29:29 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="resurgance_renderer-Jellohunter7", 
+    name="resurgence_renderer-Jellohunter7", 
     version="0.0.1",
     author="Rachit Kakkar",
-    author_email="rachitkakkar7@gmail.com",
+    author_email="",
     description="resurgance_renderer is an easy to use library used to generate 3D scenes, and can be easily scripted/extended with python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/rachitkakkar/resurgence_renderer",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
